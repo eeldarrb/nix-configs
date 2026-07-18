53ca0c0bf00e0663d89974017052aba2711f91fc
eeldarrb <167944058+eeldarrb@users.noreply.github.com>
2026-07-17 22:46:03 -0400

    move pi agent to home host

--- hosts/home/home-manager.nix
+++ hosts/home/home-manager.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    pi-coding-agent
+  ];
+
   programs.git.settings.user = {
     name = "eeldarrb";
     email = "167944058+eeldarrb@users.noreply.github.com";
--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -12,7 +12,6 @@
     neovim
     tmux
     zoxide
-    pi-coding-agent
     claude-code
     bat
     fd
